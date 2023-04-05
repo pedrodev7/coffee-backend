bdb0ec9750fd62bde5e444e934d0740cc572f83b
Pedro <pedrohenrique096k@gmail.com>
2023-04-05 18:44:34 -0300

    started project

---
+++ src/main/java/com/example/coffeeunidac/CoffeeUnidacApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeunidac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeUnidacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeUnidacApplication.class, args);
+	}
+
+}
