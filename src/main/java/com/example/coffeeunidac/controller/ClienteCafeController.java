b33074e1a22a5f3c9a2c58d11a068434a76828e8
Pedro <pedrohenrique096k@gmail.com>
2023-04-07 19:34:17 -0300

    ClienteCafe update
    Mapeando endpoint da Controller

--- src/main/java/com/example/coffeeunidac/controller/ClienteCafeController.java
+++ src/main/java/com/example/coffeeunidac/controller/ClienteCafeController.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.coffeeunidac.model.ClienteCafe;
 import com.example.coffeeunidac.service.ClienteCafeService;
 
 @RestController
+@RequestMapping("/itemCafe")
 public class ClienteCafeController {
     
     @Autowired
